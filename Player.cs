1bdf06a9043166c3c45bffcbc5032f2a06bb3b21
asd42270 <110414025+asd42270@users.noreply.github.com>
2022-09-01 17:45:50 +0900

    Swap

--- Player.cs
+++ Player.cs
@@ -6,20 +6,29 @@ public class Player : MonoBehaviour
 {
 
     public float speed;
+    public GameObject[] weapons;
+    public bool[] hasWeapons;
     float hAxis;
     float vAxis;
     bool wDown;
+    bool iDown;
+    bool sDown1;
+    bool sDown2;
+    bool sDown3;
     bool jDown;
 
     bool isJump;
     bool isDodge;
+    bool isSwap;
 
     Rigidbody rigid;
 
     Vector3 moveVec;
 
     Animator anim;
-
+    GameObject nearObject;
+    GameObject equipWeapon;
+    int equipWeaponIndex = -1;
     void Awake()
     {
         rigid = GetComponent<Rigidbody>();
@@ -35,7 +44,8 @@ public class Player : MonoBehaviour
         Turn();
         Jump();
         Dodge();
-
+        Interaction();
+        Swap();
 
     }
 
@@ -45,13 +55,18 @@ public class Player : MonoBehaviour
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
         jDown = Input.GetButtonDown("Jump");
+        iDown = Input.GetButtonDown("Interaction");
+        sDown1 = Input.GetButtonDown("Swap1");
+        sDown2 = Input.GetButtonDown("Swap2");
+        sDown3 = Input.GetButtonDown("Swap3");
     }
     void Move()
     {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
         transform.position += moveVec * speed * (wDown?0.4f:1f) * Time.deltaTime;
-
+        if(isSwap)
+            moveVec = Vector3.zero;
         anim.SetBool("isRun", moveVec != Vector3.zero);
         anim.SetBool("isWalk", wDown);
     }
@@ -94,8 +109,48 @@ public class Player : MonoBehaviour
         speed *= 0.5f;
         isDodge = false;
     }
-    
+    void Interaction()
+    {
+        if (iDown && nearObject != null && !isJump && !isDodge)
+        {
+            if (nearObject.tag == "Weapon")
+            {
+                Item item = nearObject.GetComponent<Item>();
+                int weaponIndex = item.value;
+                hasWeapons[weaponIndex] = true;
+                Destroy(nearObject);
+            }
+        }
+    }
 
+    void Swap(){
+        if(sDown1 && (!hasWeapons[0] || equipWeaponIndex == 0))
+            return;
+        if(sDown2 && (!hasWeapons[1] || equipWeaponIndex == 1))
+            return;
+        if(sDown3 && (!hasWeapons[2] || equipWeaponIndex == 2))
+            return;        
+
+        int weaponIndex = -1;
+        if (sDown1) weaponIndex = 0;
+        if (sDown2) weaponIndex = 1;
+        if (sDown3) weaponIndex = 2;
+
+        if((sDown1 || sDown2 || sDown3) && !isJump && !isDodge){
+            if (equipWeapon != null)
+                equipWeapon.SetActive(false);
+            equipWeaponIndex = weaponIndex;
+            equipWeapon = weapons[weaponIndex];
+            equipWeapon.SetActive(true);
+            anim.SetTrigger("doSwap");
+            isSwap = true;
+            Invoke("SwapeOut", 0.4f);
+        }
+    }
+    void SwapOut()
+    {
+        isSwap = false;
+    }
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Floor")
@@ -105,4 +160,25 @@ public class Player : MonoBehaviour
 
         }
     }
+
+    //플레이어의 무기 접근 인식
+    void OnTriggerStay(Collider other)
+    {
+
+        if (other.tag == "Weapon")
+        {
+            nearObject = other.gameObject;
+
+            Debug.Log(nearObject.gameObject.name);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Weapon")
+        {
+            nearObject = null;
+        }
+    }
+
 }
